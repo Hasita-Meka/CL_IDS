869dbd5e98d4191efc74f242e9a57edfac1de1a7
Hasita-Meka <148181173+Hasita-Meka@users.noreply.github.com>
2024-09-26 23:11:54 +0530

    Update EfficientNet_Derpp.py

--- EfficientNet_Derpp.py
+++ EfficientNet_Derpp.py
@@ -5,13 +5,11 @@ import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader
 from torch import nn, optim
-from torchvision.models import mobilenet_v2  # Import MobileNetV2
-from sklearn.metrics import (accuracy_score, f1_score, roc_auc_score,
-                             precision_score, recall_score, confusion_matrix)
+from torchvision.models import efficientnet_b0
+from sklearn.metrics import accuracy_score, f1_score, roc_auc_score, precision_score, recall_score, confusion_matrix
 import matplotlib.pyplot as plt
 import numpy as np
-from torchvision import datasets
-import torch.nn.functional as F
+from collections import deque
 from torchvision import datasets, transforms
 
 # Define constants
@@ -31,14 +29,14 @@ train_dir = 'Dataset/train'
 test_dir = 'Dataset/test'
 
 # Load datasets
-train_data = datasets.ImageFolder(train_dir, transform=transform)
-test_data = datasets.ImageFolder(test_dir, transform=transform)
+train_data = ImageFolder(train_dir, transform=transform)
+test_data = ImageFolder(test_dir, transform=transform)
 
 train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=True)
 test_loader = DataLoader(test_data, batch_size=BATCH_SIZE, shuffle=False)
 
-# Load MobileNetV2 model
-model = mobilenet_v2(pretrained=True)
+# Load EfficientNet model
+model = efficientnet_b0(pretrained=True)
 num_classes = len(train_data.classes)
 model.classifier[1] = nn.Linear(model.classifier[1].in_features, num_classes)  # Modify the final layer
 model = model.to(DEVICE)
@@ -48,83 +46,63 @@ criterion = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
 
-class Buffer:
-    def __init__(self, buffer_size):
-        self.buffer_size = buffer_size
-        self.data = []
-        self.labels = []
-        self.logits = []
+class Derpp:
+    """Continual learning via Dark Experience Replay++."""
 
-    def is_empty(self):
-        return len(self.data) == 0
-
-    def add_data(self, examples, labels, logits):
-        self.data.extend(examples)
-        self.labels.extend(labels)
-        self.logits.extend(logits)
-
-        if len(self.data) > self.buffer_size:
-            self.data = self.data[-self.buffer_size:]
-            self.labels = self.labels[-self.buffer_size:]
-            self.logits = self.logits[-self.buffer_size:]
-
-    def get_data(self, minibatch_size, transform, device):
-        idx = np.random.choice(len(self.data), minibatch_size, replace=False)
-        buf_inputs = torch.stack([self.data[i] for i in idx]).to(device)
-        buf_labels = torch.tensor([self.labels[i] for i in idx]).to(device)
-        buf_logits = torch.stack([self.logits[i] for i in idx]).to(device)
-        return buf_inputs, buf_labels, buf_logits
-
-
-class DerppModel:
-    def __init__(self, model, alpha=0.1, beta=0.1, buffer_size=100):
+    def __init__(self, model, criterion, optimizer, buffer_size=1000, alpha=0.5, beta=0.5):
         self.model = model
+        self.criterion = criterion
+        self.optimizer = optimizer
+        self.buffer = deque(maxlen=buffer_size)
         self.alpha = alpha
         self.beta = beta
-        self.buffer = Buffer(buffer_size)
-        self.opt = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 
     def observe(self, inputs, labels, not_aug_inputs):
-        self.opt.zero_grad()
+        self.optimizer.zero_grad()
 
         outputs = self.model(inputs)
-        loss = criterion(outputs, labels)
+        loss = self.criterion(outputs, labels)
         loss.backward()
         tot_loss = loss.item()
 
-        if not self.buffer.is_empty():
-            buf_inputs, buf_labels, buf_logits = self.buffer.get_data(BATCH_SIZE, transform, DEVICE)
-            buf_outputs = self.model(buf_inputs)
+        # Dark Experience Replay
+        if self.buffer:
+            buf_inputs, buf_labels, buf_logits = zip(*self.buffer)
+            buf_inputs = torch.stack(buf_inputs).to(DEVICE)
+            buf_labels = torch.tensor(buf_labels).to(DEVICE)
+            buf_logits = torch.stack(buf_logits).to(DEVICE)
 
-            # DER++ MSE loss
-            loss_mse = self.alpha * F.mse_loss(buf_outputs, buf_logits)
+            # MSE Loss on buffered logits
+            buf_outputs = self.model(buf_inputs)
+            loss_mse = self.alpha * nn.MSELoss()(buf_outputs, buf_logits)
             loss_mse.backward()
             tot_loss += loss_mse.item()
 
-            # DER++ Cross Entropy loss
-            buf_outputs = self.model(buf_inputs)
-            loss_ce = self.beta * criterion(buf_outputs, buf_labels)
+            # Cross-Entropy Loss on buffered labels
+            loss_ce = self.beta * self.criterion(buf_outputs, buf_labels)
             loss_ce.backward()
             tot_loss += loss_ce.item()
 
-        self.opt.step()
-        self.buffer.add_data(not_aug_inputs, labels, outputs.detach())
+        self.optimizer.step()
+
+        # Store the current inputs, labels, and logits in the buffer
+        self.buffer.extend(zip(not_aug_inputs, labels.cpu().numpy(), outputs.data.cpu()))
 
         return tot_loss
 
 
-# Function to train the model
-def train_model(model, train_loader, num_epochs):
+# Function to train the model using Derpp
+def train_model_with_derpp(model, train_loader, criterion, optimizer, num_epochs, buffer_size=1000):
+    derpp = Derpp(model, criterion, optimizer, buffer_size=buffer_size, alpha=0.5, beta=0.5)
     model.train()
     train_loss = []
-    derpp_model = DerppModel(model)
 
     for epoch in range(num_epochs):
         epoch_loss = 0.0
         for images, labels in train_loader:
             images, labels = images.to(DEVICE), labels.to(DEVICE)
-            not_aug_images = images.clone()  # Store original images for DER++
-            loss = derpp_model.observe(images, labels, not_aug_images)
+            # Note: not_aug_inputs will be same as images for this implementation
+            loss = derpp.observe(images, labels, images)
             epoch_loss += loss
 
         avg_loss = epoch_loss / len(train_loader)
@@ -133,7 +111,7 @@ def train_model(model, train_loader, num_epochs):
     return train_loss
 
 
-# Function to evaluate the model
+# Function to evaluate the model (remains the same)
 def evaluate_model(model, test_loader):
     model.eval()
     all_preds = []
@@ -187,8 +165,8 @@ def plot_metrics(train_loss, accuracy_list):
 # Start timing
 start_time = time.time()
 
-# Train the model
-train_loss = train_model(model, train_loader, NUM_EPOCHS)
+# Train the model using Derpp
+train_loss = train_model_with_derpp(model, train_loader, criterion, optimizer, NUM_EPOCHS)
 
 # Evaluate the model
 avg_loss, accuracy, f1, precision, recall, auc_roc, cm = evaluate_model(model, test_loader)
