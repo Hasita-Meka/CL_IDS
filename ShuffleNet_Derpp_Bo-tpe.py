472c2380c4061cc439e486a2fa21d24b34dd8442
Hasita-Meka <148181173+Hasita-Meka@users.noreply.github.com>
2024-10-03 11:47:19 +0530

    Update ShuffleNet_Derpp_Bo-tpe.py

--- ShuffleNet_Derpp_Bo-tpe.py
+++ ShuffleNet_Derpp_Bo-tpe.py
@@ -14,7 +14,7 @@ import optuna  # Add this import for Optuna
 from torchvision import datasets
 
 # Define constants
-BATCH_SIZE = 512
+BATCH_SIZE = 256
 NUM_EPOCHS = 30
 DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 BUFFER_SIZE = 1000  # Define buffer size
